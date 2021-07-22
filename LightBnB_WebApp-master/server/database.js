91340c07e61297021e79d3a879042ea8bc9af926
Emmanuel Etti <emmanueletti@gmail.com>
2021-07-22 22:50:19 +0000

    search button pulls requested data from database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -113,7 +113,6 @@ const getAllReservations = function (guest_id, limit = 10) {
   return pool
     .query(queryString, values)
     .then((res) => {
-      console.log(res.rows);
       return res.rows;
     })
     .catch((err) => {
@@ -132,14 +131,53 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
-  const values = [limit];
-  const queryString = ` SELECT * FROM properties LIMIT $1`;
+  const queryParams = [];
+  // create initial query string
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
 
+  // check if city option is provided
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+
+    // check for follow up filters
+    if (options['minimum_price_per_night']) {
+      queryParams.push(options['minimum_price_per_night'] * 100);
+      queryString += `AND cost_per_night >= $${queryParams.length} `;
+    }
+
+    if (options['maximum_price_per_night']) {
+      queryParams.push(options['maximum_price_per_night'] * 100);
+      queryString += `AND cost_per_night <= $${queryParams.length} `;
+    }
+
+    if (options.owner_id) {
+    }
+  }
+
+  queryString += `
+  GROUP BY properties.id `;
+
+  if (options['minimum_rating']) {
+    queryParams.push(Number(options['minimum_rating']));
+    queryString += `
+    HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `  
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 6
   return pool
-    .query(queryString, values)
-    .then((res) => {
-      return res.rows;
-    })
+    .query(queryString, queryParams)
+    .then((res) => res.rows)
     .catch((err) => {
       console.log(err);
     });
